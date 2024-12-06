c7bcf175454b145c129cb1d933c00fa5f892fc21
Juvee85 <juventino.lopez248547@potros.itson.edu.mx>
2024-12-06 07:30:10 -0700

    Setup Inicial

---
+++ AccesoDatos/src/main/java/com/mycompany/accesodatos/AccesoDatos.java
@@ -0,0 +1,12 @@
+package com.mycompany.accesodatos;
+
+/**
+ *
+ * @author juventino
+ */
+public class AccesoDatos {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ AccesoDatos/src/main/java/conexion/ConexionBD.java
@@ -0,0 +1,43 @@
+package conexion;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+
+/**
+ *
+ * @author juventino
+ */
+public class ConexionBD {
+    
+    private static MongoDatabase database;
+
+    private ConexionBD() {}
+
+    public static MongoDatabase getDatabase() {
+        if (ConexionBD.database == null) {
+            CodecRegistry pojoCodecRegistry = fromProviders(PojoCodecProvider.builder().automatic(true).build());
+
+            CodecRegistry codecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(), pojoCodecRegistry);
+
+            ConnectionString cadenaConexion = new ConnectionString("mongodb://localhost/27017");
+
+            MongoClientSettings clientsSettings = MongoClientSettings.builder()
+                    .applyConnectionString(cadenaConexion)
+                    .codecRegistry(codecRegistry)
+                    .build();
+
+            MongoClient dbServer = MongoClients.create(clientsSettings);
+
+            ConexionBD.database = dbServer.getDatabase("WaveNet");
+        }
+
+        return ConexionBD.database;
+    }
+}
